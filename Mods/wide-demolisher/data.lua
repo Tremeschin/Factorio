e9b702948ef49ba5fd1a39d494e97d88678a3ccf
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-11-21 18:45:58 -0400

    Improved wide demolisher

--- Mods/wide-demolisher/data.lua
+++ Mods/wide-demolisher/data.lua
@@ -1,20 +1,21 @@
-local scale = 1
+local scale = 4
 
-for name, demolisher in pairs(data.raw["segmented-unit"]) do
-    if string.find(name, "demolisher") then
-        for _, layer in pairs(demolisher.animation.layers) do
-            layer.scale = layer.scale * scale
-        end
-    end
-end
+-- Join the two loops together (raw["segmented-unit"] and raw["segment"])
+for entity_type, entities in pairs({
+    ["segmented-unit"] = data.raw["segmented-unit"],
+    ["segment"] = data.raw["segment"],
+}) do
+    for name, entity in pairs(entities) do
+        if string.find(name, "demolisher") then
+            for _, layer in pairs(entity.animation.layers) do
+                layer.scale = layer.scale * scale
 
-for name, segment in pairs(data.raw["segment"]) do
-    if string.find(name, "demolisher") then
-        for _, layer in pairs(segment.animation.layers) do
-            layer.scale = layer.scale * scale
+            end
 
-            for _, value in pairs(layer.shift) do
-                value = value * scale
+            for _, box in pairs(entity.collision_box) do
+                for i, value in ipairs(box) do
+                    box[i] = value * scale
+                end
             end
         end
     end
